407909341ea491ea2f329d255fde1c4a3a83d112
superman2080 <superman2080@naver.com>
2025-03-28 03:47:00 +0900

    refactor: shadow casting

---
+++ Assets/2. Scripts/ShadowPool.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+using System.Collections.Generic;
+using System.Linq;
+
+public class ShadowPool : Singleton<ShadowPool>
+{
+    public Transform Pool 
+    { 
+        get 
+        {
+            return transform;
+        } 
+    }
+
+    
+    [SerializeField] private GameObject shadowPrefab;
+
+    public bool IsShadowExisting(Shadow comp)
+    {
+        return GetChildShadowList(true).Exists((s) => s == comp);
+    }
+
+    //public bool IsExistingUsableShadow()
+    //{
+    //    return GetChildShadowList(true).Find((n) => n.gameObject.activeSelf == false);
+    //}
+
+
+    /*
+        Getting all children shadows
+        
+        Param1: Finding shadows but is active or not
+    */
+
+
+    public List<Shadow> GetChildShadowList(bool includeInactive)
+    {
+        return transform.GetComponentsInChildren<Shadow>(includeInactive).ToList();
+    }
+
+    public Shadow InstantiateShadow(ShadowCaster lightSource)
+    {
+        foreach (var shadow in GetChildShadowList(true))
+        {
+            if(shadow.gameObject.activeSelf == false)
+            {
+                shadow.gameObject.SetActive(true);
+                return shadow;
+            }
+        }
+        return Instantiate(shadowPrefab, Vector3.zero, Quaternion.identity, Pool).GetComponent<Shadow>();
+    }
+}
